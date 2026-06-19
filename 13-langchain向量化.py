fd0ca4852cff61e7d1d8df89d427980a42c22223
zhangyueping8385 <zhangyueping8385@gmail.com>
2026-06-20 02:44:57 +0800

    AI大模型RAG与智能体开发学习之路

--- 13-langchain向量化.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.embeddings import DashScopeEmbeddings
-
-model = DashScopeEmbeddings()
-
-# 单文本转向量
-print(model.embed_query("你好"))
-print(model.embed_documents(['你好', 'hello', '再见']))
